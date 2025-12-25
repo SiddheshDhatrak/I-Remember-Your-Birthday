ba8f7dbd0a678e23eb90749eff45a0a50beb7445
SiddheshDhatrak <siddheshdhatrak06@gmail.com>
2025-12-25 20:53:24 +0530

    Switch to HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// CHANGED: Import HashRouter instead of BrowserRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
 import MemoryGallery from "./components/MemoryGallery";
 import FinalMessage from "./components/FinalMessage";
@@ -9,46 +10,35 @@ import "./styles/birthday.css";
 // Background Audio Component
 const BackgroundAudio = () => {
   React.useEffect(() => {
-    // FIX: Added process.env.PUBLIC_URL to find the file on GitHub Pages
+    // Keep this PUBLIC_URL part, it is correct!
     const audio = new Audio(process.env.PUBLIC_URL + '/aud.mp3');
     audio.volume = 0.7;
     audio.loop = false;
 
-    // Attempt autoplay immediately
     const attemptAutoplay = async () => {
       try {
         await audio.play();
         console.log('Audio started playing automatically!');
-
         audio.addEventListener('ended', () => {
           console.log('Audio finished playing');
         });
       } catch (error) {
         console.log('Autoplay blocked by browser (this is normal)');
-
-        // As a last resort, try to play on first click anywhere
         const playOnFirstClick = async () => {
           try {
             await audio.play();
-            console.log('Audio started on first user interaction');
             document.removeEventListener('click', playOnFirstClick);
             document.removeEventListener('touchstart', playOnFirstClick);
           } catch (e) {
-            console.log('Even click-based play failed');
+            console.log('Click play failed');
           }
         };
-
         document.addEventListener('click', playOnFirstClick);
         document.addEventListener('touchstart', playOnFirstClick);
       }
     };
-
-    // Small delay then attempt
     setTimeout(attemptAutoplay, 500);
-
-    return () => {
-      audio.pause();
-    };
+    return () => { audio.pause(); };
   }, []);
 
   return null;
@@ -56,7 +46,6 @@ const BackgroundAudio = () => {
 
 const BirthdayPage = () => {
   React.useEffect(() => {
-    // Scroll to top on component mount
     window.scrollTo(0, 0);
   }, []);
 
@@ -74,12 +63,12 @@ const BirthdayPage = () => {
 function App() {
   return (
     <div className="App">
-      {/* This line below is PERFECT. Do not change it. */}
-      <BrowserRouter basename="/I-Remember-Your-Birthday">
+      {/* CHANGED: Switched to HashRouter. No basename needed! */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<BirthdayPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
